e389af9530ee687c94ef62edb13ce2c5d0dcf21e
prajwol subedi <prajwolsubedizzz@gmail.com>
2023-07-22 11:34:06 +0545

    route update

--- OnlineStore/src/App.jsx
+++ OnlineStore/src/App.jsx
@@ -4,7 +4,9 @@ import Cart from "./pages/Cart";
 import ProductDetail from "./pages/ProductDetails";
 import { Provider } from "react-redux";
 import store from "./store/store";
+import Error from "./pages/Error";
 import { Routes, Route, Navigate } from "react-router-dom";
+import Profile from "./pages/Profile";
 function App() {
   return (
     <Provider store={store}>
@@ -12,7 +14,9 @@ function App() {
         <Route path="/" element={<Navigate to="/products" />} />
         <Route path="/products" element={<Home />}></Route>
         <Route path="/cart" element={<Cart />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
         <Route path="/products/:id" element={<ProductDetail />}></Route>
+        <Route path="*" element={<Error />}></Route>
       </Routes>
     </Provider>
   );
