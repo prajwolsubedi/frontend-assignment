778b346d244b8fa3bc76d7bd8c778b8968f9491c
prajwol subedi <prajwolsubedizzz@gmail.com>
2023-07-22 10:29:51 +0545

    store and slice complete

--- OnlineStore/src/store/store.js
+++ OnlineStore/src/store/store.js
@@ -1,7 +1,6 @@
-import {configureStore, createSlice} from "@reduxjs/toolkit"
+import {configureStore} from "@reduxjs/toolkit"
 import productSlice from "./slice/productSlice";
 import cartSlice from "./slice/cartSlice"
-
 
 const store = configureStore({
     reducer: {
